ac76bc3bd9c57c7bafd215288a3237ec0a15e086
sohbatalquran411 <sohbatalquran411@gmail.com>
2026-07-04 18:09:22 +0300

    fix: resolve PresentationControls snap typescript error

--- components/landing/Hero3D.tsx
+++ components/landing/Hero3D.tsx
@@ -134,7 +134,7 @@ export function Hero3D() {
           <PresentationControls
             global
             config={{ mass: 2, tension: 500 }}
-            snap={{ mass: 4, tension: 1500 }}
+            snap={true}
             rotation={[0, 0, 0]}
             polar={[-Math.PI / 6, Math.PI / 6]}
             azimuth={[-Math.PI / 4, Math.PI / 4]}
